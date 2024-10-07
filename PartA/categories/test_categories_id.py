07aabd769135cbd1d4132bcc5520f5b7cca305c2
jastaggart <jasmine.taggart@mail.mcgill.ca>
2024-10-07 16:15:48 -0400

    Fixed typo in categories test

--- PartA/categories/test_categories_id.py
+++ PartA/categories/test_categories_id.py
@@ -56,13 +56,12 @@ def test_post_category_with_id_and_no_fields_filled_out(add_categories_to_databa
     assert response.json()["title"] == category_1_title
     assert response.json()["description"] == category_1_description
 
-@pytest.mark.actual_behaviour_passing
-def test_put_category_with_id_and_title_filled_out(add_categories_to_database_and_cleanup):
+def test_post_category_with_id_and_title_filled_out(add_categories_to_database_and_cleanup):
     new_category_1_id = add_categories_to_database_and_cleanup
-    response = requests.put(f'{DEFAULT_API_URL}/categories/{new_category_1_id}', json={"title": category_2_title})
+    response = requests.post(f'{DEFAULT_API_URL}/categories/{new_category_1_id}', json={"title": category_2_title})
     assert response.status_code == 200
     assert response.json()["title"] == category_2_title
-    assert response.json()["categories"][0]["description"] == category_1_description
+    assert response.json()["description"] == category_1_description
     
 def test_post_category_with_id_and_description_filled_out(add_categories_to_database_and_cleanup):
     new_category_1_id = add_categories_to_database_and_cleanup
